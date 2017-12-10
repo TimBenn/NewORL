cd377a4e16dfb42cb56bbf4dfd956848dbaab35a
Timothy Bennett <bennett.timothy.j@gmail.com>
2017-12-10 10:44:22 -0600

    New Commit

---
+++ js/map.js
@@ -0,0 +1,12 @@
+var map = new GMaps({
+    div: '#map',
+    lat: 36.196843,
+    lng: -80.381694,
+    zoom: 16
+});
+
+map.addMarker({
+    lat: 36.196843,
+    lng: -80.381694,
+    title: 'Old Richmond Lights'
+});
